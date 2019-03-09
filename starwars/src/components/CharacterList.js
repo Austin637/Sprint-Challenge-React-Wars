adecd5f45defc1e412a8e82c212b4a48c4aee0cc
Austin637 <austinesparza@gmail.com>
2019-03-09 11:57:18 -0800

    updated App to components

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Character from './Character';
 import './StarWars.css';
 
 const CharacterList = props => {
+    return ( <
+        ul >
 
+        <
+        /ul>
+    )
 }
 
 export default CharacterList;
